400015d7dd70b076787f3720e18a0b5b51a446ae
Tom Okafor <tmeka008@gmail.com>
2025-02-17 18:57:13 +0100

    create function to handle the language detection using the chrome API.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import { useEffect } from "react";
+import { handleLanguageDetector } from "./utils/handleLanguageDetector";
+
 function App() {
-  return <><h1>Text processing interface</h1></>;
+  const text = `Mochten Sie einen Text in einer anderen Sprache haben? Hier ist eine kurze Nachricht:
+
+Le soleil
+`;
+  useEffect(() => {
+    async function checkAvailability() {
+      console.log(await handleLanguageDetector(text));
+    }
+    checkAvailability();
+  }, [text]);
+  return (
+    <>
+      <h1>Text processing interface</h1>
+    </>
+  );
 }
 
 export default App;
---
+++ src/utils/handleLanguageDetector.js
@@ -0,0 +1,41 @@
+export async function handleLanguageDetector(text) {
+  // check if the language detector feature is supported
+
+  if (!("ai" in self && "languageDetector" in self.ai)) {
+    return "sorry, this feature is not supported";
+  }
+
+  // check if feature is available
+  let detector;
+  const languageDetectorCapabilities =
+    await self.ai.languageDetector.capabilities();
+  const canDetect = languageDetectorCapabilities.available;
+
+  if (canDetect === "no")
+    return "Sorry this feature is not available for your device.";
+
+  if (canDetect === "readily") {
+    detector = await self.ai.languageDetector.create();
+  } else {
+    detector = await self.ai.languageDetector.create({
+      monitor(m) {
+        m.addEventListener("downloadprogress", (e) => {
+          console.log(`Downloaded ${e.loaded} of ${e.total} bytes.`);
+        });
+      },
+    });
+  }
+
+  // get the detected language and confidence level
+  const result = (await detector.detect(text.trim()))[0];
+  const { confidence, detectedLanguage } = result;
+  const resultConfidence = (confidence * 100).toFixed(1);
+  const fullDetectedLanguage = getFullNameOfDetectedLanguage(detectedLanguage);
+  return { resultConfidence, fullDetectedLanguage };
+
+  // get the readable form of detected language from the language abbreviation
+  function getFullNameOfDetectedLanguage(language) {
+    const languageName = new Intl.DisplayNames(["en"], { type: "language" });
+    return languageName.of(language);
+  }
+}
