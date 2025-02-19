7e7271e1ecfe26fcaeb7ac12a6ef5943fff3e932
Tom Okafor <tmeka008@gmail.com>
2025-02-19 09:10:23 +0100

    set up color palette and font families.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <h1 className="">Text processing interface</h1>
+      <h1 className="text-red-thick text-5xl font-ultra">Text processing interface</h1>
     </>
   );
 }
