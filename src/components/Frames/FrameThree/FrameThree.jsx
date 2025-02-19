c2d2bd8bcd9c3979fd1078e2e20c4ca5aa8ba296
Tom Okafor <tmeka008@gmail.com>
2025-02-19 13:32:38 +0100

    Create and style content for frame three.

---
+++ src/components/Frames/FrameThree/FrameThree.jsx
@@ -0,0 +1,24 @@
+export default function FrameThree() {
+  return (
+    <section className="min-h-dvh flex flex-col gap-8 px-8 py-8 items-center justify-center">
+      <img
+        src="e-frame.png"
+        alt="person with a robot"
+        className="object-cover h-[55vh]"
+      />
+      <h1 className="text-2xl sm:text-3xl lg:text-[40px] tracking-widest leading-[150%] text-center font-bold font-ultra max-w-[30ch] text-bluish-dark antialiased">
+        AI That Speaks Every Language.
+      </h1>
+      <p className="text-lg md:text-xl text-center max-w-[50ch] font-poiret font-black tracking-wider leading-[150%]">
+        Language should never be a barrier. With
+        {" "}<span className="text-pink-thick font-black tracking-widest font-yatra text-xl md:text-2xl">
+          LinguaSwift
+        </span>
+        , you can instantly translate, summarize, and detect languages with
+        AI-powered precision. Whether you're reading foreign content,
+        communicating across cultures, or simplifying complex text, our
+        intelligent tool ensures seamless understanding—anytime, anywhere. 🌍✨
+      </p>
+    </section>
+  );
+}
--- src/components/Frames/Frames.jsx
+++ src/components/Frames/Frames.jsx
@@ -1,7 +1,11 @@
 import FrameOne from "./FrameOne/FrameOne";
+import FrameTwo from "./FrameTwo/FrameTwo";
+import FrameThree from "./FrameThree/FrameThree";
 
 export default function Frames() {
     return <div>
         <FrameOne />
+        <FrameTwo />
+        <FrameThree />
     </div>
 }
