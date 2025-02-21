885a5a11959abe6eb90920d3e0b555e9859a995b
Tom Okafor <tmeka008@gmail.com>
2025-02-21 15:52:08 +0100

    create send button component for sending user's input text.

--- src/components/ChatInterface/ChatInterfaceMain/ChatInterfaceMain.jsx
+++ src/components/ChatInterface/ChatInterfaceMain/ChatInterfaceMain.jsx
@@ -5,6 +5,7 @@ import { chatBackgrounds } from "../../../constants";
 import { handleLanguageDetector } from "../../../utils/handleLanguageDetector";
 import { handleTranslator } from "../../../utils/handleTranslator";
 import { handleSummarizer } from "../../../utils/handleSummarizer.js";
+import SendButton from "./SendButton.jsx";
 
 export default function ChatInterfaceMain() {
   const {
@@ -71,7 +72,7 @@ export default function ChatInterfaceMain() {
         ...prevState,
         {
           type: "bot",
-          message: 'Please wait. This may take a while...',
+          message: "Please wait. This may take a while...",
           detectedLanguage: null,
           detectedCode: null,
           certainty: null,
@@ -79,7 +80,7 @@ export default function ChatInterfaceMain() {
       ]);
       const summarizedText = await handleSummarizer(params);
       setChatInteractions((prevState) => [
-       ...prevState,
+        ...prevState,
         {
           type: "bot",
           message: summarizedText,
@@ -330,36 +331,10 @@ export default function ChatInterfaceMain() {
           }}
         ></textarea>
 
-        <button
-          className="absolute right-[20px] top-[50%] translate-y-[-50%] cursor-pointer"
-          onClick={() => {
-            setSelectedLanguage("en");
-            handleSubmit();
-          }}
-        >
-          <svg
-            width="45px"
-            height="45px"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M9.51002 4.23001L18.07 8.51001C21.91 10.43 21.91 13.57 18.07 15.49L9.51002 19.77C3.75002 22.65 1.40002 20.29 4.28002 14.54L5.15002 12.81C5.37002 12.37 5.37002 11.64 5.15002 11.2L4.28002 9.46001C1.40002 3.71001 3.76002 1.35001 9.51002 4.23001Z"
-              stroke={`${interfaceThemeColor}`}
-              strokeWidth="1.5"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-            <path
-              d="M5.44 12H10.84"
-              stroke={`${interfaceThemeColor}`}
-              strokeWidth="1.5"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
-        </button>
+        <SendButton
+          handleSubmit={handleSubmit}
+          setSelectedLanguage={setSelectedLanguage}
+        />
       </div>
 
       {inputError && (
---
+++ src/components/ChatInterface/ChatInterfaceMain/SendButton.jsx
@@ -0,0 +1,44 @@
+import { useContext } from "react";
+import { ChatInterfaceContext } from "../../../contexts/UseChatInterfaceContext";
+import PropTypes from "prop-types";
+
+export default function SendButton({ setSelectedLanguage, handleSubmit }) {
+  const { interfaceThemeColor } = useContext(ChatInterfaceContext);
+  return (
+    <button
+      className="absolute right-[20px] top-[50%] translate-y-[-50%] cursor-pointer"
+      onClick={() => {
+        setSelectedLanguage("en");
+        handleSubmit();
+      }}
+    >
+      <svg
+        width="45px"
+        height="45px"
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M9.51002 4.23001L18.07 8.51001C21.91 10.43 21.91 13.57 18.07 15.49L9.51002 19.77C3.75002 22.65 1.40002 20.29 4.28002 14.54L5.15002 12.81C5.37002 12.37 5.37002 11.64 5.15002 11.2L4.28002 9.46001C1.40002 3.71001 3.76002 1.35001 9.51002 4.23001Z"
+          stroke={`${interfaceThemeColor}`}
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M5.44 12H10.84"
+          stroke={`${interfaceThemeColor}`}
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
+    </button>
+  );
+}
+
+SendButton.propTypes = {
+  setSelectedLanguage: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+};
