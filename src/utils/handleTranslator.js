731c03aececa3cfc7eb86ca7254701cec7151a80
Tom Okafor <tmeka008@gmail.com>
2025-09-05 14:16:08 +0100

    refactor: update the translator handler to reflect the new method of handling the translation API per the documentation and get rid of errors related to the old code

--- src/utils/handleTranslator.js
+++ src/utils/handleTranslator.js
@@ -2,26 +2,32 @@ import { handleLanguageDetector } from "./handleLanguageDetector";
 
 export async function handleTranslator(text, targetLanguage) {
   // check if the translator api is supported
-  if (!("ai" in self && "translator" in self.ai))
-    return "Sorry this feature is not suppported.";
+  if (!("Translator" in self)) return "Sorry this feature is not suppported.";
 
   // check if the translator api is available to use
-  const translatorCapabilities = await self.ai.translator.capabilities();
-  const canTranslate = translatorCapabilities.available;
-  if (canTranslate === "no") return "Sorry this feature is not available.";
+  //   const translatorCapabilities = await self.ai.translator.capabilities();
+  //   const canTranslate = translatorCapabilities.available;
+  //   if (canTranslate === "no") return "Sorry this feature is not available.";
+
+  //   if (
+  //     translatorCapabilities.languagePairAvailable(
+  //       sourceLanguage,
+  //       targetLanguage
+  //     ) !== "readily"
+  //   )
   const { detectedLanguage: sourceLanguage } = await handleLanguageDetector(
     text
   );
-  if (
-    translatorCapabilities.languagePairAvailable(
-      sourceLanguage,
-      targetLanguage
-    ) !== "readily"
-  )
+  const checkTranslatorAvailability = await Translator.availability({
+    sourceLanguage,
+    targetLanguage,
+  });
+
+  if (checkTranslatorAvailability === "unavailable")
     return "Sorry, this translation cannot be executed.";
 
   try {
-    const translator = await self.ai.translator.create({
+    const translator = await Translator.create({
       sourceLanguage,
       targetLanguage,
     });
